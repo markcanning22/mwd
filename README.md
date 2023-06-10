bc3fcf8a0d2b9ea71f175d06d7b4c7127fb20b24
Mark Canning <markcanning22@gmail.com>
2023-06-10 12:51:08 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio projects
+
+## JSCeramics
+
+This is a project that involves building a pottery artist's website using basic CSS.
---
+++ artist-portfolio/src/App.tsx
@@ -0,0 +1,12 @@
+import React, { ReactElement } from 'react'
+import './App.pcss'
+
+const App: React.FC = (): ReactElement => {
+    return (
+        <>
+            <h1>Hello world</h1>
+        </>
+    )
+}
+
+export default App
